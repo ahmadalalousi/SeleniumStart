17dc007719c29789aa73b98c0d2fa64afa44a724
Ahmad Al Alousi <ahmad.alalousi93@gmail.com>
2019-06-13 18:48:46 -0400

    This is the first one through eclipse

--- src/day1/test1.java
+++ src/day1/test1.java
@@ -4,6 +4,8 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello test test test");
+System.out.println("hello github");
+System.out.println("message");
+//message message
 	}
 }
